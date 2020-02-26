cc783989380af594f63e4bbc7c4b7ba968c40676
Anne Pietikäinen <pietikainen.anne@gmail.com>
2020-02-26 21:16:25 +0200

    did build

---
+++ client/build/precache-manifest.ceb57fcef28d5a3bd9f45ef1a95b332b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "699daf10f234b922059c21d0993adc5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7e54bef72df7ec5a7be",
+    "url": "/static/js/2.e21edb59.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e21edb59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617e27ed62dfa5a4dd70",
+    "url": "/static/js/main.194c4817.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  }
+]);
--- server.js
+++ server.js
@@ -83,17 +83,14 @@ testIfCounterExists().then((counterExists) => {
 })
 // getAndAddCounterValue().then((value) => console.log(value))
 
-/* if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+/* if (process.env.NODE_ENV === 'production') {*/
+app.use(express.static('client/build'))
 
-  const path = require('path');
-  app.get('*', (req,res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+const path = require('path')
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 
-} */
+//res.sendFile(path.join(__dirname, '../client/build/index.html'))
 
-
-
-app.use(express.static('./client/public'))
 const server = app.listen(port, () => console.log(`Listening on port ${port}`))
